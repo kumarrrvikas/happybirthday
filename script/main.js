bcc57443968fd0dccbe30e1906f529296f6b66d4
Rainie <nguyen@tolanguyen.us.to>
2025-03-07 12:03:50 +0000

    Pending changes exported from your codespace

--- script/main.js
+++ script/main.js
@@ -1,12 +1,12 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'E có mún xem thứ gì trong đây không?',
         icon: 'warning',
-        showCancelButton: true,
+        showCancelButton: false,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
+        confirmButtonText: 'Chắc là có á.',
         cancelButtonText: 'No',
     }).then((result) => {
         if (result.isConfirmed) {
